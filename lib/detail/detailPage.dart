93b6f39545acd6d0d9a458cc2b6f3d7fd2b0952e
sya-hid <syarifhidayatullah2020@gmail.com>
2022-01-13 12:02:08 +0700

    update remdme

--- lib/detail/detailPage.dart
+++ lib/detail/detailPage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:mountain_trip/models/mountain.dart';
 import 'package:mountain_trip/models/user.dart';
 import 'package:mountain_trip/theme.dart';
 import 'package:smooth_star_rating/smooth_star_rating.dart';
@@ -86,185 +85,182 @@ class Body1 extends StatelessWidget {
         ),
       ),
       Container(
-          // height: 350.0,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            gradient: LinearGradient(
-              begin: FractionalOffset.topCenter,
-              end: FractionalOffset.bottomCenter,
-              colors: [
-                Colors.transparent,
-                Colors.black87,
-              ],
-              stops: [0.0, 0.95],
-            ),
+        padding: EdgeInsets.fromLTRB(20, 80, 20, 20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          gradient: LinearGradient(
+            begin: FractionalOffset.topCenter,
+            end: FractionalOffset.bottomCenter,
+            colors: [
+              Colors.transparent,
+              Colors.black,
+            ],
+            stops: [0.0, 1.0],
           ),
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 80),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
               children: [
-                Row(
-                  children: [
-                    Text(
-                      '12º',
-                      style: GoogleFonts.roboto().copyWith(
-                          fontSize: 48,
-                          color: kTextColor,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      width: 15,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'SW 5km/h',
-                          style: GoogleFonts.roboto().copyWith(
-                              fontSize: 24,
-                              color: kTextColor,
-                              fontWeight: FontWeight.normal),
-                        ),
-                        Text(
-                          '63.9830ºN,19.0670ºW',
-                          style: GoogleFonts.roboto().copyWith(
-                              fontSize: 14,
-                              color: kTextColor,
-                              fontWeight: FontWeight.normal),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-                SizedBox(height: 250),
                 Text(
-                  Get.arguments['name'],
+                  '12º',
                   style: GoogleFonts.roboto().copyWith(
+                      fontSize: 48,
                       color: kTextColor,
-                      fontSize: 22,
-                      letterSpacing: 5,
                       fontWeight: FontWeight.bold),
                 ),
-                SizedBox(height: 10),
-                Row(
+                SizedBox(
+                  width: 15,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    SmoothStarRating(
-                        allowHalfRating: true,
-                        onRated: (v) {},
-                        starCount: 5,
-                        rating: 4.5,
-                        size: 20.0,
-                        isReadOnly: true,
-                        defaultIconData: Icons.star_outline_rounded,
-                        filledIconData: Icons.star_rounded,
-                        halfFilledIconData: Icons.star_half_rounded,
-                        color: kTextColor,
-                        borderColor: kTextColor,
-                        spacing: 0.0),
-                    SizedBox(width: 10),
                     Text(
-                      "4.5 Reviews",
+                      'SW 5km/h',
+                      style: GoogleFonts.roboto().copyWith(
+                          fontSize: 24,
+                          color: kTextColor,
+                          fontWeight: FontWeight.normal),
+                    ),
+                    Text(
+                      '63.9830ºN,19.0670ºW',
+                      style: GoogleFonts.roboto().copyWith(
+                          fontSize: 14,
+                          color: kTextColor,
+                          fontWeight: FontWeight.normal),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Spacer(),
+            Text(
+              Get.arguments['name'],
+              style: GoogleFonts.roboto().copyWith(
+                  color: kTextColor,
+                  fontSize: 22,
+                  letterSpacing: 5,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                SmoothStarRating(
+                    allowHalfRating: true,
+                    onRated: (v) {},
+                    starCount: 5,
+                    rating: 4.5,
+                    size: 20.0,
+                    isReadOnly: true,
+                    defaultIconData: Icons.star_outline_rounded,
+                    filledIconData: Icons.star_rounded,
+                    halfFilledIconData: Icons.star_half_rounded,
+                    color: kTextColor,
+                    borderColor: kTextColor,
+                    spacing: 0.0),
+                SizedBox(width: 10),
+                Text(
+                  "4.5 Reviews",
+                  style: GoogleFonts.roboto().copyWith(
+                      color: kTextColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Difficulty',
+                      style: GoogleFonts.roboto().copyWith(
+                          color: kTextColor,
+                          fontSize: 18,
+                          letterSpacing: 2,
+                          fontWeight: FontWeight.normal),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      '70%',
                       style: GoogleFonts.roboto().copyWith(
                           color: kTextColor,
                           fontSize: 16,
                           fontWeight: FontWeight.normal),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Difficulty',
-                          style: GoogleFonts.roboto().copyWith(
-                              color: kTextColor,
-                              fontSize: 18,
-                              letterSpacing: 2,
-                              fontWeight: FontWeight.normal),
-                        ),
-                        SizedBox(height: 5),
-                        Text(
-                          '70%',
-                          style: GoogleFonts.roboto().copyWith(
-                              color: kTextColor,
-                              fontSize: 16,
-                              fontWeight: FontWeight.normal),
-                        )
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Distance',
-                            style: GoogleFonts.roboto().copyWith(
-                              color: kTextColor,
-                              fontSize: 18,
-                              letterSpacing: 2,
-                              fontWeight: FontWeight.normal,
-                            )),
-                        SizedBox(height: 5),
-                        Text(
-                          '24 Km',
-                          style: GoogleFonts.roboto().copyWith(
-                              color: kTextColor,
-                              fontSize: 16,
-                              fontWeight: FontWeight.normal),
-                        )
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Elevation',
-                          style: GoogleFonts.roboto().copyWith(
-                              color: kTextColor,
-                              fontSize: 18,
-                              letterSpacing: 2,
-                              fontWeight: FontWeight.normal),
-                        ),
-                        SizedBox(height: 5),
-                        Text(
-                          '3000 m',
-                          style: GoogleFonts.roboto().copyWith(
-                              color: kTextColor,
-                              fontSize: 16,
-                              fontWeight: FontWeight.normal),
-                        )
-                      ],
                     )
                   ],
                 ),
-                SizedBox(height: 20),
-                Text(
-                  Get.arguments['desc'],
-                  maxLines: 5,
-                  style: GoogleFonts.roboto().copyWith(
-                      color: kTextColor,
-                      fontSize: 14,
-                      height: 1.5,
-                      letterSpacing: 2,
-                      fontWeight: FontWeight.normal),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Travelers(
-                      users: topTravelers,
-                    ),
-                    Button(),
+                    Text('Distance',
+                        style: GoogleFonts.roboto().copyWith(
+                          color: kTextColor,
+                          fontSize: 18,
+                          letterSpacing: 2,
+                          fontWeight: FontWeight.normal,
+                        )),
+                    SizedBox(height: 5),
+                    Text(
+                      '24 Km',
+                      style: GoogleFonts.roboto().copyWith(
+                          color: kTextColor,
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal),
+                    )
                   ],
                 ),
-                // SizedBox(height: 20),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Elevation',
+                      style: GoogleFonts.roboto().copyWith(
+                          color: kTextColor,
+                          fontSize: 18,
+                          letterSpacing: 2,
+                          fontWeight: FontWeight.normal),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      '3000 m',
+                      style: GoogleFonts.roboto().copyWith(
+                          color: kTextColor,
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal),
+                    )
+                  ],
+                )
               ],
             ),
-          ))
+            SizedBox(height: 20),
+            Text(
+              Get.arguments['desc'],
+              maxLines: 5,
+              style: GoogleFonts.roboto().copyWith(
+                  color: kTextColor,
+                  fontSize: 14,
+                  height: 1.5,
+                  letterSpacing: 2,
+                  fontWeight: FontWeight.normal),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Travelers(
+                  users: topTravelers,
+                ),
+                Button(),
+              ],
+            ),
+          ],
+        ),
+      )
     ]);
   }
 }
@@ -277,35 +273,21 @@ class Button extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      // height: 50,
-      // width: MediaQuery.of(context).size.width * 0.5,
-      padding: const EdgeInsets.all(15),
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
       decoration: BoxDecoration(
-        // color: kPrimaryColor.withOpacity(0.5),
-        borderRadius: BorderRadius.circular(10),
+        borderRadius: BorderRadius.circular(15),
+        color: kPrimaryColor.withOpacity(0.65),
       ),
-      child: Material(
-        color: kPrimaryColor.withOpacity(0.8),
-        child: InkWell(
-          onTap: () {},
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  "Start Route",
-                  style: TextStyle(
-                      color: Colors.white,
-                      letterSpacing: 1.5,
-                      fontWeight: FontWeight.normal,
-                      fontSize: 18),
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
+      child: FlatButton(
+          onPressed: () {},
+          child: Text(
+            'Start Route',
+            style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 1.5,
+                fontWeight: FontWeight.normal,
+                fontSize: 18),
+          )),
     );
   }
 }
